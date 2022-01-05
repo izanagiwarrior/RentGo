6c004bf99219fd61be28b536370dd122e0618486
David William Da Costa <55936252+david-william19@users.noreply.github.com>
2022-01-05 23:48:30 +0900

    landing page layout

--- resources/views/users/welcome.blade.php
+++ resources/views/users/welcome.blade.php
@@ -27,37 +27,7 @@
         <div class="row no-gutters">
             <div class="col-md-12	featured-top">
                 <div class="row no-gutters">
-                    <div class="col-md-4 d-flex align-items-center">
-                        <form action="#" class="request-form ftco-animate bg-primary">
-                            <h2>Make your trip</h2>
-                            <div class="form-group">
-                                <label for="" class="label">Pick-up location</label>
-                                <input type="text" class="form-control" placeholder="City, Airport, Station, etc">
-                            </div>
-                            <div class="form-group">
-                                <label for="" class="label">Drop-off location</label>
-                                <input type="text" class="form-control" placeholder="City, Airport, Station, etc">
-                            </div>
-                            <div class="d-flex">
-                                <div class="form-group mr-2">
-                                    <label for="" class="label">Pick-up date</label>
-                                    <input type="text" class="form-control" id="book_pick_date" placeholder="Date">
-                                </div>
-                                <div class="form-group ml-2">
-                                    <label for="" class="label">Drop-off date</label>
-                                    <input type="text" class="form-control" id="book_off_date" placeholder="Date">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="" class="label">Pick-up time</label>
-                                <input type="text" class="form-control" id="time_pick" placeholder="Time">
-                            </div>
-                            <div class="form-group">
-                                <input type="submit" value="Rent A Car Now" class="btn btn-secondary py-3 px-4">
-                            </div>
-                        </form>
-                    </div>
-                    <div class="col-md-8 d-flex align-items-center">
+                    <div class="col-md d-flex align-items-center">
                         <div class="services-wrap rounded-right w-100">
                             <h3 class="heading-section mb-4">Better Way to Rent Your Perfect Cars</h3>
                             <div class="row d-flex mb-4">
@@ -86,7 +56,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <p><a href="#" class="btn btn-primary py-3 px-4">Reserve Your Perfect Car</a></p>
+                            <p class="text-center"><a href="{{route('car')}}" class="btn btn-primary py-3 px-4">Reserve Your Perfect Car</a></p>
                         </div>
                     </div>
                 </div>
